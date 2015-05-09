8e6db344cd1ca6f4c0c9057e8d6bbba5fbc21508
banhtieu <banhtieu@Trans-MacBook-Air.local>
2015-05-09 16:04:08 +0700

    - Correct Error in Admin.

--- src/main/resources/static/admin/controllers/Application.js
+++ src/main/resources/static/admin/controllers/Application.js
@@ -3,7 +3,7 @@
  */
 
 angular
-    .module('TravelApplication', ['ngMaterial', 'ngMdIcons', 'ngRoute'])
+    .module('TravelApplication', ['ngMaterial', 'ngMdIcons', 'ngRoute', 'textAngular'])
     .controller('ApplicationController', function ($scope, $mdSidenav) {
         $scope.toggleSidenav = function(menuId) {
             $mdSidenav(menuId).toggle();
@@ -12,10 +12,13 @@ angular
     .config(['$routeProvider', function($routeProvider){
         $routeProvider
             .when('/tours', {
-                templateURL: 'views/tours.html',
+                templateUrl: '/admin/views/tours.html',
                 controller: 'TourController'
+            })
+            .when('/tours/add', {
+                templateUrl: '/admin/views/editTour.html'
             })
             .otherwise({
                 redirectTo: '/tours'
-            })
+            });
     }]);
